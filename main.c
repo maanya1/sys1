f1e5436a3f4ea71a67079a79a4b4262bf2cb1ff3
Maanya Tandon <MaanyaTandon@Maanyas-MBP.fios-router.home>
2020-03-30 13:28:34 -0400

    second commit

--- main.c
+++ main.c
@@ -7,6 +7,32 @@
 
 #include "flags.h"
 #include "tokens.h"
+typedef struct direct{
+  
+}
+
+void listFilesRecursive(char *basePath){
+  char path[1000];
+  struct direct *dp;
+  DIR *dir = opendir(basePath);
+
+  //Unable to open directory stream
+  if(!dir)
+    return;
+
+  while((dp = readdir(dir)) != NULL){
+    if(strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0){
+      printf("%s\n", dp->d_name);
+      //construct new path from our base path
+      strcpy(path, basePath);
+      strcat(path, "/");
+      strcat(path, dp->d_name);
+
+      listFilesRecursive(path);
+    }
+  }
+  closeddir(dir);
+}
 
 int main(int argc, char** argv) {
   Flags* flags = get_flags(argc, argv);
