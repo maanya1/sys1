7e84d5871d218cd2d313957c6ccbf38bba65fd93
Maanya Tandon <MaanyaTandon@Maanyas-MBP.fios-router.home>
2020-03-30 16:43:27 -0400

    changes to heapify

---
+++ heap.h
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+typedef struct HeapNode {
+    int frequency;
+    char* word;
+
+    struct HeapNode* left;
+    struct HeapNode* right;;
+} HeapNode;
+
+typedef struct Heap{
+    int *arr;
+    int count;
+    int capacity;
+} Heap;
+
+Heap *CreateHeap(int capacity,int heap_type);
+HeapNode* create_heap_node(int frequency, char* word);
+void insert(Heap *h, int key);
+
+
+
+
